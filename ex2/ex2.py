1960b6ff679246bdf110d7d71ca9b247e097d8e3
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-01 18:31:42 +0530

    ex2 commit

---
+++ ex2/ex2.py
@@ -0,0 +1,8 @@
+#hello gaurav,this is comment this line does not execute in the program
+#if we want to ingonr any line in our code then we can use comment
+
+print "hello this is testing"   #this is also comment after this line.
+
+#this line is also not execute.
+
+print "this will run" 
