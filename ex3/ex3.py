63fa67752f287ab8d2211af2610a1d30b75a40d1
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-01 18:35:39 +0530

    ex3 commit

---
+++ ex3/ex3.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens"
+
+print "Hens", 25 + 30 /6
+
+print "Now i will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "is it try 3+2>3-2"
+
+print 3+2 < 3-2
+
+print "what is 3+2", 3+2
+
+print "what is 3-2 ",3-2
+
+print "ohhh that's why it's false."
+
+print "How about some more."
+
+print "Is it grater?", 5 > -2
+
+print "is it grater or equal?", 5>= -2
+print "is it less or equal?", 5 <= -2
