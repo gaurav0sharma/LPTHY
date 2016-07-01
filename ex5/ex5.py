c080434d625400241c0db59c0966e90e80a06d1b
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-01 18:39:52 +0530

    ex5 commit

---
+++ ex5/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Gaurav Sharma'
+my_age = 24    #years
+my_height = "5'9" #feet
+my_weight = 55 #kg :P
+my_eye = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "lat's talk about %s" % my_name
+print "He's %s feet tall" % my_height
+print "he's %d kg heavy" % my_weight
+print "Actully that's not too havy."
+print "He's got %s eyes and %s hair"%(my_eye,my_hair)
+print "His teeth are usually %s depending on the coffee" %my_teeth
+print "If I add %d %s and %d I get %d" % (my_age,my_height,my_weight,my_age+my_weight)
+print "%r" %my_age
+my_weight_in_gram=my_weight*1000
+print "weight in gram %r" % my_weight_in_gram #%r is use of anything means we can print a string or numric value also 
