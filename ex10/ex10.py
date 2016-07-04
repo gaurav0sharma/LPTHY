9ac345cda1290ab16269bb041ed2475ccd842cbf
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-04 19:11:45 +0530

    ex10 commit

---
+++ ex10/ex10.py
@@ -0,0 +1,14 @@
+print "I am 5'9\" tall"
+tabby_cat = "\t I'm tabbed in "
+persian_cat = "I'm split\non a line."
+backslsh_cat = "I'm \\ a \\ cat."
+fat_cat ="""
+I'll do a list:
+\t* cat food
+\t* fishies
+\t* catnip\n\t*grass"""
+
+print tabby_cat
+print persian_cat
+print backslsh_cat
+print fat_cat
