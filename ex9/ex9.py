c301621198fa874f2a7270991037c144ea269ffc
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-04 19:09:50 +0530

    ex9 commit

---
+++ ex9/ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff,remeber type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\njan\nfeb\nmar\napr\nmay\njun\njul\naug\nsept\n"
+print "++"*40
+print " here some days %s" % months
+print " here some days %r" % months
+print "++"*40
+print "here are the days: ",days
+print "here are the months: ",months
+print """there's something going on here.
+With the three double-quotes.
+we'll be able to type as much as we like.
+Even 4 line if er want,or 5, or 6."""
