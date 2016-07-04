b90a6e7c9f6624d63febf6792846418fd08bdc6e
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-04 18:52:59 +0530

    ex7 commit

---
+++ ex7/ex7.py
@@ -0,0 +1,23 @@
+print "marry had a little lamb."
+print "Its fleece was white as %s" % 'snow'
+print "And everywhere that mary went went."
+print "." * 10 #i think is print this line 10 times but m not sure.
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "p"
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+print "=" * 25
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
