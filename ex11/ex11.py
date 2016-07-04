795d9ee4b697d9df317254577fbde67a4d81810d
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-04 19:13:11 +0530

    ex11 commit

---
+++ ex11/ex11.py
@@ -0,0 +1,14 @@
+# asking Questions
+
+print "How old are you?"
+age = raw_input()
+print "How tall are you?"
+height = raw_input()
+print "How much do you weight?"
+weight = raw_input()
+
+print "so, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+print "="*40
+x = input("enter any value")  #"give integer value or like this -> "g" then it's working."
+print x*70
