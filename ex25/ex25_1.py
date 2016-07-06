fcfd5549523762a62a972ee434305346768fdd4a
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-06 17:56:44 +0530

    ex25 commit

---
+++ ex25/ex25_1.py
@@ -0,0 +1,32 @@
+import ex25
+sentence = "All good things come to those who wait."
+
+words = ex25.break_words(sentence)
+
+print "=="*10
+words
+print "=="*10
+sorted_words = ex25.sort_words(words)
+print "=="*10
+#sorted_words
+print "=="*10
+#ex25.print_first_word(words)
+print "=="*10
+#ex25.print_last_word(words)
+print "=="*10
+#words
+print "=="*10
+#ex25.print_first_word(sorted_words)
+print "=="*10
+#ex25.print_last_word(sorted_words)
+print "=="*10
+#sorted_words
+print "=="*10
+#sorted_words = ex25.sort_sentence(sentence)
+print "=="*10
+print "=="*10
+#sorted_words
+print "=="*10
+#ex25.print_first_and_last(sentence)
+print "=="*10
+#ex25.print_first_and_last_sorted(sentence)
