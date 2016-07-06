883a75fd80c17e1ac7b9640eb1d6347cf3fe8957
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-06 10:21:55 +0530

    ex13 commit

---
+++ ex13/ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script,first,second,third = argv
+
+print "the scrpit is called %s" %   script
+print "1st argument is             ",first
+print "2nd argument is",second
+print "3rd argument is",third
+
+#when we use the % in print, we must use format specifiers
