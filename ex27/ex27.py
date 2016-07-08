2aa84d7f0855d5a0527ff87f1ef7f2e21780ab01
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-08 10:19:14 +0530

    ex27 commit

---
+++ ex27/ex27.py
@@ -0,0 +1,33 @@
+
+a=5
+b=4
+c=5
+
+if a == c:
+	print "in the if"
+
+if a == b or a == c:
+	print "and condition"
+
+if a == b and a == c:
+	print "condition false"
+
+if a == b or a != b:	
+	print "hello"
+
+if  1:
+	print "one in if"
+
+if 0:
+	print "if 0"
+
+if not 1:
+	print "not 1"
+
+print True + True
+
+if True:
+	print "True"
+
+if False:
+	print "False"
