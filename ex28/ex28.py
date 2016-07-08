b273a8fc4e1df4c3fd81c42cead1cb079bf54cc5
gaurav0sharma <gaurav.sharma@izeltech.com>
2016-07-08 10:22:09 +0530

    ex28 commit

---
+++ ex28/ex28.py
@@ -0,0 +1,30 @@
+if True and True:
+	print "True and True:"
+
+if False and True:
+	print "false and true"
+
+if 1==1 and 2==1:
+	print "1==1 and 2==1"
+
+if "test" == "test":
+	print "test == test"
+
+if 1==1 or 2 != 1:
+	print "1=1 and 1!=2"
+
+if True and 1 == 1:
+	print "true and 1==1"
+
+if "test" == "testing":
+	print "test = = testinh"
+
+	
+if "test" == 1:
+	print"test==1"
+
+if not(True and False):
+	print "not(true and false)"
+
+if "chunky" == "bacon" and (not (3 == 4 or 3 == 3)):
+	print "true"
